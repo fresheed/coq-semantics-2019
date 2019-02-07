c393facd34902da93945d281c9c0f046af358420
Podkopaev Anton <podkoav239@gmail.com>
2019-02-07 11:54:45 +0300

    Changed notation to avoid collision w/ Hahn.

--- src/Id.v
+++ src/Id.v
@@ -6,25 +6,25 @@ Require Import Omega.
 Inductive id : Type :=
   Id : nat -> id.
 
-Reserved Notation "m <<= n" (at level 70, no associativity).
-Reserved Notation "m >>  n" (at level 70, no associativity).
-Reserved Notation "m <<  n" (at level 70, no associativity).
+Reserved Notation "m i<= n" (at level 70, no associativity).
+Reserved Notation "m i>  n" (at level 70, no associativity).
+Reserved Notation "m i<  n" (at level 70, no associativity).
 
 Inductive le_id : id -> id -> Prop :=
-  le_conv : forall n m, n <= m -> (Id n) <<= (Id m)
-where "n <<= m" := (le_id n m).   
+  le_conv : forall n m, n <= m -> (Id n) i<= (Id m)
+where "n i<= m" := (le_id n m).   
 
 Inductive lt_id : id -> id -> Prop :=
-  lt_conv : forall n m, n < m -> (Id n) << (Id m)
-where "n << m" := (lt_id n m).   
+  lt_conv : forall n m, n < m -> (Id n) i< (Id m)
+where "n i< m" := (lt_id n m).   
 
 Inductive gt_id : id -> id -> Prop :=
-  gt_conv : forall n m, n > m -> (Id n) >> (Id m)
-where "n >> m" := (gt_id n m).   
+  gt_conv : forall n m, n > m -> (Id n) i> (Id m)
+where "n i> m" := (gt_id n m).   
 
-Notation "n <<= m" := (le_id n m).
-Notation "n >>  m" := (gt_id n m).
-Notation "n <<  m" := (lt_id n m).
+Notation "n i<= m" := (le_id n m).
+Notation "n i>  m" := (gt_id n m).
+Notation "n i<  m" := (lt_id n m).
 
 Ltac prove_with th :=
   intros; 
@@ -34,14 +34,14 @@ Ltac prove_with th :=
   try match goal with H: {_} + {_} |- _ => inversion_clear H end;
   repeat
     match goal with 
-      H: ?n <  ?m |-  _                + {Id ?n << Id ?m}  => right
+      H: ?n <  ?m |-  _                + {Id ?n i< Id ?m}  => right
     | H: ?n <  ?m |-  _                + {_}               => left
-    | H: ?n >  ?m |-  _                + {Id ?n >> Id ?m}  => right
+    | H: ?n >  ?m |-  _                + {Id ?n i> Id ?m}  => right
     | H: ?n >  ?m |-  _                + {_}               => left
-    | H: ?n <  ?m |- {_}               + {Id ?n << Id ?m}  => right
-    | H: ?n <  ?m |- {Id ?n << Id ?m}  + {_}               => left
-    | H: ?n >  ?m |- {_}               + {Id ?n >> Id ?m}  => right
-    | H: ?n >  ?m |- {Id ?n >> Id ?m}  + {_}               => left
+    | H: ?n <  ?m |- {_}               + {Id ?n i< Id ?m}  => right
+    | H: ?n <  ?m |- {Id ?n i< Id ?m}  + {_}               => left
+    | H: ?n >  ?m |- {_}               + {Id ?n i> Id ?m}  => right
+    | H: ?n >  ?m |- {Id ?n i> Id ?m}  + {_}               => left
     | H: ?n =  ?m |-  _                + {Id ?n =  Id ?m}  => right
     | H: ?n =  ?m |-  _                + {_}               => left
     | H: ?n =  ?m |- {_}               + {Id ?n =  Id ?m}  => right
@@ -51,20 +51,20 @@ Ltac prove_with th :=
     | H: ?n <> ?m |- {_}               + {Id ?n <> Id ?m}  => right
     | H: ?n <> ?m |- {Id ?n <> Id ?m}  + {_}               => left
 
-    | H: ?n <= ?m |-  _                + {Id ?n <<= Id ?m} => right
+    | H: ?n <= ?m |-  _                + {Id ?n i<= Id ?m} => right
     | H: ?n <= ?m |-  _                + {_}               => left
-    | H: ?n <= ?m |- {_}               + {Id ?n <<= Id ?m} => right
-    | H: ?n <= ?m |- {Id ?n <<= Id ?m} + {_}               => left
+    | H: ?n <= ?m |- {_}               + {Id ?n i<= Id ?m} => right
+    | H: ?n <= ?m |- {Id ?n i<= Id ?m} + {_}               => left
     end;
   try (constructor; assumption); congruence.
 
-Lemma lt_eq_lt_id_dec: forall (id1 id2 : id), {id1 << id2} + {id1 = id2} + {id2 << id1}.
+Lemma lt_eq_lt_id_dec: forall (id1 id2 : id), {id1 i< id2} + {id1 = id2} + {id2 i< id1}.
 Proof. prove_with lt_eq_lt_dec. Qed.
 
-Lemma gt_eq_gt_id_dec: forall (id1 id2 : id), {id1 >> id2} + {id1 = id2} + {id2 >> id1}.
+Lemma gt_eq_gt_id_dec: forall (id1 id2 : id), {id1 i> id2} + {id1 = id2} + {id2 i> id1}.
 Proof. prove_with gt_eq_gt_dec. Qed.
 
-Lemma le_gt_id_dec : forall id1 id2 : id, {id1 <<= id2} + {id1 >> id2}.
+Lemma le_gt_id_dec : forall id1 id2 : id, {id1 i<= id2} + {id1 i> id2}.
 Proof. prove_with le_gt_dec. Qed.
 
 Lemma eq_dec : forall n m : nat, {n = m} + {n <> m}.
@@ -87,7 +87,7 @@ Proof.
 Qed.
 
 Lemma lt_gt_id_false : forall id1 id2 : id,
-  id1 >> id2 -> id2 >> id1 -> False.
+  id1 i> id2 -> id2 i> id1 -> False.
 Proof.
   intros id1 id2 H1 H2. 
   inversion H1. inversion H2. 
@@ -96,7 +96,7 @@ Proof.
 Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id,
-  id2 <<= id1 -> id2 >> id1 -> False.
+  id2 i<= id1 -> id2 i> id1 -> False.
 Proof.
   intros id1 id2 H1 H2. 
   inversion H1. inversion H2. 
@@ -105,7 +105,7 @@ Proof.
 Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, 
-  id1 <<= id2 -> {id1 = id2} + {id2 >> id1}.
+  id1 i<= id2 -> {id1 = id2} + {id2 i> id1}.
 Proof.
   intros id1 id2 H. remember (gt_eq_gt_id_dec id1 id2). inversion s. inversion H0. 
     apply (lt_gt_id_false id1 id2) in H1. contradiction. 
@@ -115,7 +115,7 @@ Proof.
 Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id,
-  id1 <> id2 -> {id1 >> id2} + {id2 >> id1}.
+  id1 <> id2 -> {id1 i> id2} + {id2 i> id1}.
 Proof.
   intros id1 id2 H. unfold not in H. remember (le_gt_id_dec id1 id2). 
     inversion s. apply le_lt_eq_id_dec in H0. inversion H0. contradiction. 
@@ -123,7 +123,7 @@ Proof.
 Qed.
     
 Lemma eq_gt_id_false : forall id1 id2 : id,
-  id1 = id2 -> id1 >> id2 -> False.
+  id1 = id2 -> id1 i> id2 -> False.
 Proof.
   intros id1 id2 H1 H2. 
   inversion H2. rewrite <-H3 in H1. rewrite <-H0 in H1. inversion H1. omega.
