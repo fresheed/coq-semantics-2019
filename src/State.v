4a2de133e827148681993e699db66eb38a6162b0
Podkopaev Anton <podkoav239@gmail.com>
2019-02-21 09:28:42 +0300

    Extended lemmas in State.v.

--- src/State.v
+++ src/State.v
@@ -6,6 +6,8 @@ Require Import Omega.
 Require Export Arith Arith.EqNat.
 Require Export Id.
 
+From hahn Require Import HahnBase.
+
 Section S.
 
   Variable A : Set.
@@ -23,51 +25,65 @@ Section S.
 
   Notation "st [ x '<-' y ]" := (update st x y) (at level 0).
 
-  Lemma state_deterministic: forall (st : state) (x : id) (n m : A),
-    st / x => n -> st / x => m -> n = m.
+  Lemma state_deterministic (st : state) (x : id) (n m : A)
+        (SN : st / x => n)
+        (SM : st / x => m) :
+    n = m.
   Proof.
-    intros st x n m H1 H2. induction st. inversion H1.
-      inversion H1. inversion H2; congruence.
-                    inversion H2. congruence. apply IHst; assumption. 
+    induction st; inversion SN; subst.
+    all: inversion SM; desf.
+    apply IHst; desf.
   Qed.    
 
-  Lemma update_eq : forall (st : state) (x : id) (n : A),
+  Lemma update_eq (st : state) (x : id) (n : A) :
     st [x <- n] / x => n.
+  Proof. unfold update. constructor. Qed.
+
+  Lemma update_neq (st : state) (x2 x1 : id) (n m : A)
+        (NEQ : x2 <> x1) : st / x1 => m <-> st [x2 <- n] / x1 => m.
   Proof.
-    intros st x n. unfold update. constructor.
+    split; intros HH.
+    { unfold update. apply st_binds_tl; auto. }
+    unfold update in HH. inv HH.
   Qed.
 
-  Lemma update_neq : forall (st : state) (x2 x1 : id) (n m : A),
-    x2 <> x1 -> st / x1 => m -> st [x2 <- n] / x1 => m.
+  Lemma update_shadow (st : state) (x1 x2 : id) (n1 n2 m : A) :
+    st[x2 <- n1][x2 <- n2] / x1 => m <-> st[x2 <- n2] / x1 => m.
   Proof.
-    intros st x2 x1 n m H1 H2. unfold update. apply st_binds_tl; auto.
+    split; intros HH.
+    { unfold update in HH.
+      inv HH.
+      { apply update_eq. }
+      inv H5. apply update_neq; auto. }
+    inv HH.
+    { apply update_eq. }
+    repeat (apply update_neq; auto).
   Qed.
 
-  Lemma update_shadow : forall (st : state) (x1 x2 : id) (n1 n2 m : A),
-    st[x2 <- n1][x2 <- n2] / x1 => m -> st[x2 <- n2] / x1 => m.
+  Lemma update_same (st : state) (x1 x2 : id) (n1 m : A)
+        (SN : st / x1 => n1)
+        (SM : st / x2 => m) :
+    st [x1 <- n1] / x2 => m.
   Proof.
-    intros st x1 x2 n1 n2 m H. unfold update in *. set (id_eq_dec x2 x1). inversion_clear s.
-      rewrite H0 in *. inversion H. constructor. unfold not in H6. exfalso. auto.
-      inversion H. constructor. apply st_binds_tl. assumption. inversion H7. exfalso. auto. assumption.
+    destruct (id_eq_dec x1 x2) as [|NEQ]; subst.
+    2: by apply update_neq.
+    assert (m = n1); subst.
+    2: by apply update_eq.
+    eapply state_deterministic; eauto.
   Qed.
 
-  Lemma update_same : forall (st : state) (x1 x2 : id) (n1 m : A),
-    st / x1 => n1 -> st / x2 => m -> st [x1 <- n1] / x2 => m.
-  Proof.
-    intros st x1 x2 n1 m H1 H2. unfold update. set (id_eq_dec x1 x2). inversion_clear s.
-      rewrite H in *. apply (state_deterministic st x2 n1 m) in H1. rewrite H1. constructor. assumption.
-        apply st_binds_tl. congruence. assumption.    
-  Qed.
-
-  Lemma update_permute : forall (st : state) (x1 x2 x3 : id) (n1 n2 m : A),
-    x2 <> x1 -> 
-    st [x2 <- n1][x1 <- n2] / x3 => m ->
+  Lemma update_permute (st : state) (x1 x2 x3 : id) (n1 n2 m : A)
+        (NEQ : x2 <> x1)
+        (SM : st [x2 <- n1][x1 <- n2] / x3 => m) :
     st [x1 <- n2][x2 <- n1] / x3 => m.
   Proof.
-    intros st x1 x2 x3 n1 n2 m H1 H2. unfold update in *.
-    inversion H2. apply st_binds_tl. rewrite <-H5. congruence. apply st_binds_hd.
-    inversion H7. apply st_binds_hd. apply st_binds_tl. assumption. 
-      apply st_binds_tl; assumption.       
+    destruct (id_eq_dec x1 x3) as [|NN]; subst.
+    { apply update_neq; auto.
+      inv SM. apply update_eq. }
+    destruct (id_eq_dec x2 x3) as [|AA]; subst.
+    { inv SM. inv H5. apply update_eq. }
+    repeat (apply update_neq; auto).
+    inv SM. inv H5.
   Qed.  
 
 End S.
